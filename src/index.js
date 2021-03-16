9678b906976f1d930da5345fea82e5498852df52
Phil Haines <phil.haines92@gmail.com>
2021-03-16 14:19:57 -0600

    refactor practice

--- src/index.js
+++ src/index.js
@@ -1,87 +1,75 @@
-const cardContainer = document.getElementById('card-container')
-const addColorForm = document.getElementById('add-color-form')
+const baseUrl = 'http://localhost:3000/colors';
+const cardContainer = document.getElementById("card-container");
+const addColorForm = document.getElementById("add-color-form");
 
+fetch(baseUrl)
+	.then(parseJson)
+	.then((colors) => {
+		colors.forEach((color) => {
+			const colorCard = document.createElement("div");
+			colorCard.className = "color-card";
+			colorCard.style.background = `${color.hex}`;
 
-const baseURL = `http://localhost:3000/colors`;
+			const colorName = document.createElement("h2");
+			colorName.innerText = color.name;
 
-fetch(baseURL)
-    .then(parseJson)
-    .then(iterateOverColors)
+			const voteCounter = document.createElement("p");
+			voteCounter.innerText = ` Votes ${color.votes}`;
+
+			const voteButton = document.createElement("button");
+			voteButton.textContent = "+1 Vote";
+			voteButton.addEventListener("click", () => {
+				color.votes++;
+				fetch(`${baseUrl}/${color.id}`, {
+					method: "PATCH",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						votes: color.votes,
+					}),
+				});
+				voteCounter.innerText = ` Votes ${color.votes}`;
+			});
+
+			const deleteButton = document.createElement("button");
+			deleteButton.innerHTML = "&times;";
+
+			deleteButton.addEventListener("click", () => {
+				colorCard.remove();
+				fetch(`${baseUrl}/${color.id}`, {
+					method: "DELETE",
+				});
+			});
+
+			colorCard.append(colorName, voteCounter, voteButton, deleteButton);
+			cardContainer.append(colorCard);
+		});
+	});
 
 function parseJson(response) {
 	return response.json();
 }
 
-function iterateOverColors(colors){
-    colors.forEach(renderColors)
-    addColor(colors);
+const addNewColor = () => {
+	addColorForm.addEventListener("submit", (e) => {
+		e.preventDefault;
+
+		const formData = new FormData(e.target);
+		const colorName = formData.get("name");
+		const colorHex = formData.get("hex");
+
+		fetch(`${baseUrl}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				name: colorName,
+				hex: colorHex,
+				votes: 0,
+			}),
+		});
+	});
 }
-
-function renderColors(color) {
-        const colorCard = document.createElement('div')
-        colorCard.className = "color-card"
-        colorCard.style.backgroundColor = ` ${color.hex}`
-
-
-        const colorName = document.createElement('h2')
-        colorName.innerText = color.name
-
-
-        const voteCounter = document.createElement('p')
-        voteCounter.textContent = `${color.votes} Votes`;
-        const voteButton = document.createElement('button')
-        voteButton.innerText = '+1 Vote'
-        voteButton.addEventListener('click', (e) => {
-            color.votes++
-            fetch(`${baseURL}/${color.id}`, {
-                method: 'PATCH',
-                headers: {
-                    "Content-Type": "application/JSON"
-                },
-                body: JSON.stringify({
-                    votes: color.votes
-                }),
-            })
-            voteCounter.textContent = `${color.votes} Votes`;
-        })
-
-        const deleteButton = document.createElement('button')
-        deleteButton.className = 'delete-button'
-        deleteButton.innerHTML = '&times;'
-
-        deleteButton.addEventListener('click', () => {
-            colorCard.remove()
-            fetch(`${baseURL}/${color.id}`, {
-                method: 'DELETE',
-            });
-        })
-
-        colorCard.append(colorName, voteCounter, voteButton, deleteButton)
-        cardContainer.append(colorCard)
-}
-
-
-        function addColor(color) {
-			addColorForm.addEventListener("submit", (e) => {
-				e.preventDefault();
-				const addColorFormData = new FormData(e.target);
-				const colorName = addColorFormData.get('name');
-                console.log(colorName)
-				const colorHex = addColorFormData.get("hex");
-                console.log(colorHex)
-				fetch(`${baseURL}`, {
-					method: "POST",
-					header: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						name: colorName,
-						hex: colorHex,
-						votes: 0
-					}),
-				});
-                
-			});
-		}
-
-
+addNewColor();
